d9b84771f839798bd1d72570a63a90922adbcced
anwesag2 <anwesag2@illinois.edu>
2020-10-24 13:01:34 -0700

    want to test scan codes

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,13 +1,16 @@
 #include "keyboard.h"
 
-static char scan_codes[NUM_KEYS]= 	{'\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\0',
-	 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\0', '\0', 'a', 's',
-	 'd', 'f', 'g', 'h', 'j', 'k', 'l' , ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
-	 'b', 'n', 'm',',', '.', '/', '\0', '*', '\0', ' ', '\0'};     //scan_code array inputs from keyboard
-
 /* SOURCES:  https://wiki.osdev.org/PS/2_Keyboard
     some code from Linux documentation of PS/2 Keyboard
 */
+
+static char scan_codes[NUM_KEYS]= 	{ //presses
+    '\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\0',
+	 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '\0', 'a', 's',
+	 'd', 'f', 'g', 'h', 'j', 'k', 'l' , ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
+	 'b', 'n', 'm',',', '.', '/', '\0', '*', '\0', ' ', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
+     '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'
+     };     
 
 /* initialize_keyboard
  * 
@@ -30,10 +33,9 @@ void initialize_keyboard(){
  * Return value: None
  */ 
 void key_board_handler(){   
-       
-    
-    if(inb(KEYBOARD_PORT) < NUM_KEYS && inb(KEYBOARD_PORT) >= 0){   //check if scan code is in bounds of scan code array
-        putc(scan_codes[inb(KEYBOARD_PORT)]);   //print character to screen
+    switch(inb(KEYBOARD_PORT)){
+        default:
+            putc(scan_codes[inb(KEYBOARD_PORT)]);   //print character to screen
     }
     send_eoi(KEYBOARD_IRQ);  //stop interrupt on pin
     
---
+++ student-distrib/terminal.c
@@ -0,0 +1,14 @@
+#include "terminal.h"
+
+uint32_t terminal_read (uint32_t fd, void* buf, uint32_t nbytes){
+
+}
+uint32_t terminal_write (uint32_t fd, const void* buf, uint32_t nbytes){
+
+}
+uint32_t terminal_open (const uint8_t* filename){
+
+}
+uint32_t terminal_close (uint32_t fd){
+
+}
