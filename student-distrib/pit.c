df35833cf1325fa54a2e07898d0551e7dc84750e
Smith <ncsmith2@illinois.edu>
2020-11-29 23:44:24 -0600

    no interrupts after switching terminals. glitches

--- student-distrib/pit.c
+++ student-distrib/pit.c
@@ -29,23 +29,31 @@ void pit_handler() {
 }
 
 void schedule() {
+    if(scheduled_terminal == visible_terminal) { //scheduled terminal is the same as the visible terminal
+        //set video memory
+        pageTable[VIDEO_MEMORY_IDX >> 12] = (VIDEO_MEMORY_IDX | 0x003); // 0x3 are bits needed to set present, rw, supervisor
+    }
+    else {
+        pageTable[VIDEO_MEMORY_IDX >> 12] = VIDEO_MEMORY_IDX + ((0x1000*(scheduled_terminal + 1)) | 0x003);
+    }
+    flush_tlb();
+    //copy video memory from previous process into corresponding video memory backup
+    // switch(scheduled_terminal){
+    //     case 0:
+    //         memcpy((void *)BACKUP_ONE, (void *)VIDEO_MEMORY_IDX, 0x1000);
+    //         break;
+    //     case 1:
+    //         memcpy((void *)BACKUP_TWO, (void *)VIDEO_MEMORY_IDX, 0x1000);
+    //         break;
+    //     case 2:
+    //         memcpy((void *)BACKUP_THREE, (void *)VIDEO_MEMORY_IDX, 0x1000);
+    //         break;
+    //     default:
+    //         break;
+    // }
+
     //save eip from the previous process to the pcb for that process
     curr_pcb->eip = save_eip;
-
-    //copy video memory from previous process into corresponding video memory backup
-    switch(scheduled_terminal){
-        case 0:
-            memcpy((void *)BACKUP_ONE, (void *)VIDEO_MEMORY_IDX, 0x1000);
-            break;
-        case 1:
-            memcpy((void *)BACKUP_TWO, (void *)VIDEO_MEMORY_IDX, 0x1000);
-            break;
-        case 2:
-            memcpy((void *)BACKUP_THREE, (void *)VIDEO_MEMORY_IDX, 0x1000);
-            break;
-        default:
-            break;
-    }
 
     //increment scheduled terminal number
     scheduled_terminal++;
@@ -71,22 +79,43 @@ void schedule() {
 }
 
 void switch_terminal(uint32_t terminal_num){
+    send_eoi(0x01);
     cli();
+    memcpy(VIDEO_MEMORY_IDX + ((0x1000*(visible_terminal + 1)) | 0x003), VIDEO_MEMORY_IDX, 0x1000);
     visible_terminal = terminal_num;
+    memcpy(VIDEO_MEMORY_IDX, VIDEO_MEMORY_IDX + ((0x1000*(terminal_num + 1)) | 0x003), 0x1000);
+    if(active_processes[visible_terminal] == NULL) { //if never opened terminal before
+        //clear();
+        execute((uint8_t *)"shell");
+    }
+    else{
+        //switch to user program
+        curr_pcb = (PCB_t *)(START_OF_KERNEL_STACKS - (active_processes[scheduled_terminal]->process_id)*SIZE_OF_KERNEL_STACK);
+        
+        
+        switch_task_memory();
+        load_program_into_memory(curr_pcb->filename);
+        prepare_context_switch();
+        //push the iret context and iret to the scheduled process
+        push_iret_context(curr_pcb->eip);
+    }
     
-    if(scheduled_terminal == terminal_num) { //scheduled terminal is the same as the visible terminal
-        //set video memory
-        pageTable[VIDEO_MEMORY_IDX >> 12] = (VIDEO_MEMORY_IDX | 0x003); // 0x3 are bits needed to set present, rw, supervisor
-    }
-    else {
-        //pageDirectory[0] = ((uint32_t)pageTable | 0x003); // 0x3 are bits needed to set present, rw, supervisor
-        pageTable[VIDEO_MEMORY_IDX >> 12] = VIDEO_MEMORY_IDX + ((0x1000*(9)) | 0x003);
-        flush_tlb();
-        if(active_processes[visible_terminal] == NULL) { //if never opened terminal before
-            execute((uint8_t *)"shell");
-        }
+
+    
+
+    // if(scheduled_terminal == terminal_num) { //scheduled terminal is the same as the visible terminal
+    //     //set video memory
+    //     pageTable[VIDEO_MEMORY_IDX >> 12] = (VIDEO_MEMORY_IDX | 0x003); // 0x3 are bits needed to set present, rw, supervisor
+    // }
+    // else {
+    //     //pageDirectory[0] = ((uint32_t)pageTable | 0x003); // 0x3 are bits needed to set present, rw, supervisor
+    //     pageTable[VIDEO_MEMORY_IDX >> 12] = VIDEO_MEMORY_IDX + ((0x1000*(visible_terminal + 1)) | 0x003);
+    //     flush_tlb();
+    //     if(active_processes[visible_terminal] == NULL) { //if never opened terminal before
+    //         execute((uint8_t *)"shell");
+    //     }
       
-    }
-    sti();
+    // }
+    
 }
  
